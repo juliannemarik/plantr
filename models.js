362b219cf5e6ed88e277ff5875fb3dd7b1c5ae96
Julianne Crawford <julianne.marik@gmail.com>
2018-09-24 21:01:00 -0500

    plantr complete

--- models.js
+++ models.js
@@ -1,5 +1,57 @@
 const Sequelize = require("sequelize");
+const db= new Sequelize("postgres://localhost:5432/plantr", {logging : false });
 
-const db = new Sequelize("postgres://localhost:5432/plantr", {});
+// DEFINE GARDENER MODEL
+const Gardener = db.define('gardener', {
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
 
-module.exports = db;
+// DEFINE PLOT MODEL
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
+
+// DEFINE VEGETABLE MODEL
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  plantedOn: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+})
+
+// ESTABLISH ASSOCIATIONS
+Plot.belongsTo(Gardener); // Plot belongs to a gardener
+Gardener.hasOne(Plot); // Gardener has one plot
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'}); // Vegetable may belong to many plots
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'}); // A plot has many vegetables
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+
+
+// DEFINE VEGETABLE PLOT MODEL
+const vegetablePlot = db.model('vegetable_plot');
+
+
+// EXPORT DB & MODELS
+module.exports = { db, Gardener, Plot, Vegetable, vegetablePlot };
--- seed.js
+++ seed.js
@@ -1,9 +1,94 @@
-const db = require('./models');
+const seedModels = require('./models');
+const db = seedModels.db;
+const Gardener = seedModels.Gardener;
+const Plot = seedModels.Plot;
+const Vegetable = seedModels.Vegetable;
+const vegetablePlot = seedModels.vegetablePlot;
+let vegetables, gardeners, plots;
 
 db.sync({ force: true })
   .then(() => {
+    const vegetableData = [
+      {
+        name: 'carrot',
+        color: 'orange',
+        plantedOn: new Date()
+      },
+      {
+        name: 'tomato',
+        color: 'red',
+        plantedOn: new Date()
+      },
+      {
+        name: 'corn',
+        color: 'yellow',
+        plantedOn: new Date()
+      }
+    ]
+    return Vegetable.bulkCreate(vegetableData, { returning: true });
+  }).then((createdVegetables) => {
+    vegetables = createdVegetables;
+    const [carrot, tomato, corn] = vegetables;
+    const gardenerData = [
+      {
+        name: 'William',
+        age: 24,
+        favoriteVegetableId: carrot.id
+      },
+      {
+        name: 'Julianne',
+        age: 25,
+        favoriteVegetableId: tomato.id
+      },
+      {
+        name: 'Cati',
+        age: 27,
+        favoriteVegetableId: corn.id
+      }
+    ]
+    return Gardener.bulkCreate(gardenerData, { returning: true });
+  }).then((createdGardeners) => {
+    gardeners = createdGardeners;
+    const [william, julianne, cati] = gardeners;
+    const plotData = [
+      {
+        size: 10,
+        shaded: false,
+        gardenerId: william.id
+      },
+      {
+        size: 100,
+        shaded: true,
+        gardenerId: julianne.id
+      },
+      {
+        size: 40,
+        shaded: false,
+        gardenerId: cati.id
+      }
+    ]
+    return Plot.bulkCreate(plotData, { returning: true })
+  }).then((createdPlots) => {
+    plots = createdPlots;
+    const [plot1, plot2, plot3] = plots;
+    const [carrot, tomato, corn] = vegetables
+    const vegetablePlotData = [
+      {
+        vegetableId: carrot.id,
+        plotId: plot1.id
+      },
+      {
+        vegetableId: tomato.id,
+        plotId: plot2.id
+      },
+      {
+        vegetableId: corn.id,
+        plotId: plot3.id
+      }
+    ]
+    return vegetablePlot.bulkCreate(vegetablePlotData, { returning: true })
+  }).then(() => {
     console.log('Database synced!');
-    // db.close() - if without 'finally'
   })
   .catch(err => {
     console.log('Error occurred!');
@@ -12,3 +97,6 @@ db.sync({ force: true })
   .finally(() => {
     db.close();
   });
+
+
+
